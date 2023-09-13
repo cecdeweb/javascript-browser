83e29480860a3577362a2c5e91b006c49d51f495
cecdeweb <deweb.bordeaux@gmail.com>
2023-09-13 09:08:53 +0200

    modif 1

--- 03 - form-manipulation/js/exercices.js
+++ 03 - form-manipulation/js/exercices.js
@@ -1,10 +1,1 @@
-// Solution simple
-//let selectDiv = document.querySelector("#button");
-//selectDiv.innerHTML = "<button>Mon bouton</button>";
-// Solution avc createElement()
-let createButton = document.createElement('button');
-document.querySelector('#button').appendChild(createButton);
-createButton.innerHTML('Mon button');
-//let selectDiv = document.querySelector('#button');
-//selectDiv.appendChild(elementButton);
-//document.querySelector('button').innerHTML('Mon button');
+// Your code goes here
--- 04 - animations/js/exercices.js
+++ 04 - animations/js/exercices.js
@@ -1,3 +1,1 @@
 // Your code goes here
-let selectP = document.querySelector(".style1");
-selectP.remove(".style2");
